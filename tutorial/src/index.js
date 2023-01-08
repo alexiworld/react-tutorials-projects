771423b52be9f4f1cbf9b90782e1d246eb963705
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-08 09:57:14 -0800

    27. Props - Basic Setup - part 2

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -8,30 +8,23 @@ const author = 'Colleen Hoover';
 const title = 'It Ends with Us';
 const img = './images/book.jpg';
 
-// parameters
-const someFunc = (param1, param2) => {
-  console.log(param1, param2);
-};
-// arguments
-someFunc('job', 'developer');
-
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book job='developer' />
+      <Book title='random title' number={22} />
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author.toUpperCase()}</h4>
+      {console.log(props)}
     </article>
   );
 };
