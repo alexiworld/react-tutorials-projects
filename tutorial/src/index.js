3a69f91bd8918ceeaffcbe7d8be505da96598f71
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-08 13:52:03 -0800

    33. Key Prop

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -9,11 +9,13 @@ const books = [
     author: 'Colleen Hoover',
     title: 'It Ends with Us',
     img: './images/book.jpg',
+    id: 1,
   },
   {
     author: 'E2M Chef Connect, Jennie Casselman, Andres Chaparro',
     title: 'Eager 2 Cook, Healthy Recipes for Healthy Living: Beef & Poultry',
     img: 'https://images-na.ssl-images-amazon.com/images/I/612Ko7Ps0uL._AC_UL600_SR600,400_.jpg',
+    id: 2,
   },
 ];
 
@@ -21,8 +23,8 @@ const BookList = () => {
   return (
     <section className='booklist'>
       {books.map((book) => {
-        const { author, title, img } = book;
-        return <Book author={author} title={title} img={img} />;
+        const { author, title, img, id } = book;
+        return <Book author={author} title={title} img={img} key={id} />;
       })}
     </section>
   );
