808e7f8274a627bf5c5099f95419a6720d2f24c6
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-08 15:39:06 -0800

    36. Form Submission

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -44,17 +44,25 @@ const BookList = () => {
 };
 
 const EventExamples = () => {
-  const handleFormInput = () => {
+  const handleFormInput = (e) => {
     console.log('handle form input');
+    console.log(e.target);
+    console.log(e.target.name);
+    console.log(e.target.value);
   };
 
   const handleButtonClick = () => {
     alert('handle button click');
   };
 
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+  };
+
   return (
     <section>
-      <form>
+      <form onSubmit={handleFormSubmission}>
         <h2>Typical Form</h2>
         <input
           type='text'
