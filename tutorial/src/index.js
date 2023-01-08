8fb31098f83d2dcf410970d48d2463dd4dfd2aff
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-07 19:24:52 -0800

    13-25 Intro, First Component, JSX Rules, BookList, CSS, Local Images, JSX-CSS

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 
-function Greeting() {
-    return <h2>My First Component</h2>;
-}
+import './index.css';
 
-export default Greeting;
+//https://amazon.com/Best-Sellers-Books/zgbs/books/
+
+const BookList = () => {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+//https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg
+
+const Image = () => <img src='./images/book.jpg' alt='It Ends with Us' />;
+const Title = () => <h2>It Ends with Us</h2>;
+const Author = () => {
+  return (
+    <h4 style={{ color: '#617', fontSize: '0.75rem', marginTop: '0.5rem' }}>
+      Colleen Hoover
+    </h4>
+  );
+};
 
 const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<Greeting/>);
+root.render(<BookList />);
