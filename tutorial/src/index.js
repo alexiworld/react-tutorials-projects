39ab3dd18fe359b498bb1151d4b2c58387cdd886
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-07 19:36:56 -0800

    26 JSX - Java Script

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -16,28 +16,19 @@ const BookList = () => {
   );
 };
 
+const author = 'Colleen Hoover';
 const Book = () => {
+  const title = 'It Ends with Us';
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src='./images/book.jpg' alt='It Ends with Us' />
+      <h2>{title}</h2>
+      <h4>{author.toUpperCase()}</h4>
     </article>
   );
 };
 
 //https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg
-
-const Image = () => <img src='./images/book.jpg' alt='It Ends with Us' />;
-const Title = () => <h2>It Ends with Us</h2>;
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: '#617',
-    fontSize: '0.75rem',
-    marginTop: '0.5rem',
-  };
-  return <h4 style={inlineHeadingStyles}>Colleen Hoover</h4>;
-};
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(<BookList />);
