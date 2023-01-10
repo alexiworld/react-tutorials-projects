00eb180a6519a9d58166c97b21ca08733790d259
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-09 21:47:19 -0800

    46. Numbers Challenge

--- tutorial/src/Book.js
+++ tutorial/src/Book.js
@@ -1,15 +1,18 @@
 // children is a special property
 const Book = (props) => {
-  const { author, img, title, children } = props;
+  const { author, img, title, number, children } = props;
   // ERROR: onClick={getBook(id)} will be run at page load time.
   // This is because every time we have () after the function it
   // makes it run immediately. How to work around it? Well we can
   // use the arrow function as shown below.
+  // `# ${number + 1}` is called template literal. This is nice
+  // way of adding # before then actual number.
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author.toUpperCase()}</h4>
+      <span className='number'>{`# ${number + 1}`}</span>
       {children}
     </article>
   );
