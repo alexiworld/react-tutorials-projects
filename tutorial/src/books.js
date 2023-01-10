6dbaf9a793d5749d513ca36805e363b6193ea127
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-09 21:27:53 -0800

    45. Local Images (src folder). These images get optimized when building a production ready code

--- tutorial/src/books.js
+++ tutorial/src/books.js
@@ -1,14 +1,28 @@
+// According to John Smilga importing images (stored under src
+// in our case) is faster than loading images from public (see
+// public/images folder).
+
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
 export const books = [
   {
     author: 'Colleen Hoover',
     title: 'It Ends with Us',
-    img: './images/book.jpg',
+    img: img1,
     id: 1,
   },
   {
     author: 'E2M Chef Connect, Jennie Casselman, Andres Chaparro',
     title: 'Eager 2 Cook, Healthy Recipes for Healthy Living: Beef & Poultry',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/612Ko7Ps0uL._AC_UL600_SR600,400_.jpg',
+    img: img2,
     id: 2,
+  },
+  {
+    author: 'Jennette McCurdy',
+    title: "I'm Saddened that My Mom Died",
+    img: img3,
+    id: 3,
   },
 ];
