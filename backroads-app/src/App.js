65fb81f021c878ab05fa47f971ff1694aba0b5a2
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-10 05:40:02 -0800

    53. SRC Boilerplate. Remove the boilerplate.

--- backroads-app/src/App.js
+++ backroads-app/src/App.js
@@ -1,9 +1,4 @@
-import './App.css';
-
 function App() {
-  // Showing that  <React.StrictMode> will cause the app to be rendered twice.
-  // Strict mode runs only in DEV and help to detect errors during development time
-  console.log('Hello! Be seeing it twice.');
   return <h1>backroads app</h1>;
 }
 
