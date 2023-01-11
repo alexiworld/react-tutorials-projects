08a7f9f7e3ef23c71f25d803e32faf67ae30cd41
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-11 04:43:08 -0800

    65. Alternative Approach - Page Links Component

--- backroads-app/src/components/Footer.js
+++ backroads-app/src/components/Footer.js
@@ -1,20 +1,10 @@
 import { pageLinks, socialLinks } from '../data';
+import PageLinks from './PageLinks';
 
 const Footer = () => {
   return (
     <footer className='section footer'>
-      <ul className='footer-links'>
-        {pageLinks.map((link) => {
-          const { id, href, text } = link;
-          return (
-            <li key={id}>
-              <a href={href} className='footer-link'>
-                {text}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
+      <PageLinks parentClass='footer-links' itemClass='footer-link' />
       <ul className='footer-icons'>
         {socialLinks.map((link) => {
           const { id, href, icon } = link;
@@ -29,7 +19,7 @@ const Footer = () => {
       </ul>
       <p className='copyright'>
         copyright &copy; Backroads travel tours company
-        <span id='date'></span> all rights reserved
+        <span id='date'>{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
   );
--- backroads-app/src/components/Navbar.js
+++ backroads-app/src/components/Navbar.js
@@ -1,6 +1,7 @@
 /* add dynamic import */
 import logo from '../images/logo.svg';
-import {pageLinks, socialLinks} from '../data';
+import { pageLinks, socialLinks } from '../data';
+import PageLinks from './PageLinks';
 
 const Navbar = () => {
   return (
@@ -12,34 +13,25 @@ const Navbar = () => {
             <i className='fas fa-bars'></i>
           </button>
         </div>
-        {/*<!-- left this comment on purpose -->*/}
-        <ul className='nav-links' id='nav-links'>
-        {pageLinks.map((link) => {
-            return (
-              <li key={link.id}>
-                <a href={link.href} className='nav-link'>
-                  {` ${link.text} `}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+        <PageLinks parentClass='nav-links' itemClass='nav-link' />
 
         {/*  Using target="_blank" without rel="noreferrer" (which implies rel="noopener") is a security risk in older browsers: see https://mathiasbynens.github.io/rel-noopener/#recommendations  react/jsx-no-target-blank */}
-        
+
         <ul className='nav-icons'>
           {socialLinks.map((link) => {
-            const {id, href, icon} = link;
+            const { id, href, icon } = link;
             return (
               <li key={id}>
-                <a href={href} 
-                target='_blank' 
-                rel="noreferrer" 
-                className='nav-icon'>
+                <a
+                  href={href}
+                  target='_blank'
+                  rel='noreferrer'
+                  className='nav-icon'
+                >
                   <i className={icon}></i>
                 </a>
               </li>
-            ); 
+            );
           })}
         </ul>
       </div>
