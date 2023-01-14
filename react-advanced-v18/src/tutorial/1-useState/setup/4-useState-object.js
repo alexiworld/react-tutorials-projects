3a94d3fb7c0958dd6f096de41177615463589ace
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-14 09:23:17 -0800

    78. use State - Object Example

--- react-advanced-v18/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-v18/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message',
+  });
+  //console.log(person);
+  const changeMessage = () => {
+    // next line will work but wipe out all other data
+    // setPerson({message: 'new message'});
+
+    // next lines won't work as react component will not be updated
+    // person.message = 'new message';
+    // setPerson(person);
+
+    // what will work is creating a new object using the spread operator
+    // to copy the fields from the old object, and override the message field.
+    setPerson({ ...person, message: 'new message' });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className='btn' onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
