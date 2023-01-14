f438f7c60d7cfb03461d2c1e01d5bdb38e659db3
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-14 09:27:10 -0800

    79. useState - Multiple State Values

--- react-advanced-v18/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-v18/src/tutorial/1-useState/setup/4-useState-object.js
@@ -6,24 +6,19 @@ const UseStateObject = () => {
     age: 24,
     message: 'random message',
   });
-  //console.log(person);
+
+  const [name, setName] = useState('peter');
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState('random message');
+
   const changeMessage = () => {
-    // next line will work but wipe out all other data
-    // setPerson({message: 'new message'});
-
-    // next lines won't work as react component will not be updated
-    // person.message = 'new message';
-    // setPerson(person);
-
-    // what will work is creating a new object using the spread operator
-    // to copy the fields from the old object, and override the message field.
-    setPerson({ ...person, message: 'new message' });
+    setMessage('new message');
   };
   return (
     <>
-      <h3>{person.name}</h3>
-      <h3>{person.age}</h3>
-      <h3>{person.message}</h3>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
       <button className='btn' onClick={changeMessage}>
         change message
       </button>
