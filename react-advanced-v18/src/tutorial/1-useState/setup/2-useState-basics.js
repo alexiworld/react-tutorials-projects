140d07e2d4190b6ddbf1ad5277485713b842ae79
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-14 08:32:03 -0800

    75. useState - Basics. Use 'useState' function to change the title, which in previous video failed.

--- react-advanced-v18/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced-v18/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,9 +1,26 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  console.log(useState); // show it is a function
-  console.log(useState()); // show it returns an array
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  // destructure the returned array. the naming convention is
+  // to name the function 'set' and the name of the variable,
+  // which in our case is 'text'
+  const [text, setText] = useState('random title'); // value can be array, number, string, boolean e.g. 109
+  const handleClick = () => {
+    setText('hello people');
+  };
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className='btn' onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
