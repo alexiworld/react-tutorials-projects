96ddc2934d36432c2a7d7bc8ee3fd4468f02fc14
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-14 10:40:40 -0800

    81. Functional Update Form. Use prevState to prevent racing conditions or passing an older value to function scheduled to be run after some time.

--- react-advanced-v18/src/tutorial/1-useState/setup/5-useState-counter.js
+++ react-advanced-v18/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -5,6 +5,24 @@ const UseStateCounter = () => {
   const reset = () => {
     setValue(0);
   };
+  const complexIncrease = () => {
+    // The problem is with double or tripple clicking of the button
+    // as the final result will not be the value +2 or +3 but will
+    // be +1. Why? Because these invokations of complexIncrease are
+    // so close in time (< 2000ms) so they all pick up the same
+    // value e.g. 2 and increase it by 1, resulting in all them
+    // setting up 3 as a state value.
+    // setTimeout(() => {
+    //   setValue(value + 1);
+    // }, 2000);
+
+    // So what is the proper solution then:
+    setTimeout(() => {
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
   return (
     <>
       <section style={{ margin: '4rem 0' }}>
@@ -17,6 +35,13 @@ const UseStateCounter = () => {
           reset
         </button>
         <button className='btn' onClick={() => setValue(value + 1)}>
+          increase
+        </button>
+      </section>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>
           increase
         </button>
       </section>
