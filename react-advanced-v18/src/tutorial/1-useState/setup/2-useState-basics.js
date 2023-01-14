400168f6ffef96d0e4352a40b35456de79aa1f91
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-14 08:19:53 -0800

    75. useState - Basics. useState function printed to the log

--- react-advanced-v18/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced-v18/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
+  console.log(useState); // show it is a function
+  console.log(useState()); // show it returns an array
   return <h2>useState basic example</h2>;
 };
 
