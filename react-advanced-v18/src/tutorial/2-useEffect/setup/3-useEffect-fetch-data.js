1e0cceab346f765533756485288ae3a4f1fcb331
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-15 20:42:46 -0800

    87. useEffect - Fetch Data. Add async function getUsers.

--- react-advanced-v18/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ react-advanced-v18/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -5,14 +5,14 @@ const url = 'https://api.github.com/users';
 const UseEffectFetchData = () => {
   const [users, setUsers] = useState([]);
 
-  // Cannot use async in useEffect. You can either setup
-  // async in the useEffect callback functions or outside
-  // which is what the course instructor exactly will do.
-  // Trying to run the code as is now (with async) will
-  // result in seeing "react-dom.development.js:86 Warning:
-  // useEffect must not return anything besides a function,
-  // which is used for clean-up." in the browser console.
-  useEffect(async () => {});
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    console.log(users);
+  };
+  useEffect(() => {
+    getUsers();
+  });
   return (
     <>
       <h3>github users</h3>
