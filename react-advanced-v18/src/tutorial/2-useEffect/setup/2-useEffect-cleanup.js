7aa1f48a791823cf78b5efe873a54b8bd33c0dd2
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-15 20:16:10 -0800

    86. useEffect - Cleanup Function. Found a problem with multiple window size listeners being registered, and the size grows with every window resize. Propose own solution by using the second parameter of useEffect and so registering the event listener only during the initial rendering. This however is not the cleanup function.

--- react-advanced-v18/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ react-advanced-v18/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -6,7 +6,29 @@ import React, { useState, useEffect } from 'react';
 const UseEffectCleanup = () => {
   const [size, setSize] = useState(window.innerWidth);
   console.log(size);
-  return <h2>useEffect cleanup</h2>;
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  // [] is required to prevent adding a new event listener every
+  // time.
+  // to see the problem, resize the window a few times, seeing the
+  // new widths in the console log being printed out on every
+  // change, and then go to the Elements tab of Dev Tools, and Event
+  // Listeners subtab. There you will under 'resize' node see
+  // multiple Window event listeners with the line 10 (of the
+  // checkSize) being listed.
+  useEffect(() => {
+    window.addEventListener('resize', checkSize);
+  }, []);
+
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} px</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
