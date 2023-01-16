5ba58cab1bccb366062fab1bf64c52c2834d8107
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-15 22:31:09 -0800

    90. Short-Circuit Evaluation. A different way of JSX component rendering if text is not empty.

--- react-advanced-v18/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-v18/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState(''); // peter
   const firstValue = text || 'hello world';
   const secondValue = text && 'hello world';
 
@@ -13,6 +13,7 @@ const ShortCircuit = () => {
       <h1>value: {secondValue}</h1>
       {/* {if (true) {console.log('hello world');}} */}
       <h1>{text || 'john doe'}</h1>
+      {text && <h1>hello world</h1>}
     </>
   );
 };
