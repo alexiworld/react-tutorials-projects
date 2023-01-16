24e8511e96291153550cbfa3fc8f5b02df8a1f60
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-15 20:56:02 -0800

    87. useEffect - Fetch Data. Set users and render them. Note the [] as 2nd param of useEffect.

--- react-advanced-v18/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ react-advanced-v18/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -8,14 +8,37 @@ const UseEffectFetchData = () => {
   const getUsers = async () => {
     const response = await fetch(url);
     const users = await response.json();
+    // setUsers(users) // this is correct but there is a different
+    // problem. what is the problem? what is setState doing? It
+    // preserves the value and triggers rerender. what do you think
+    // will happen if we call setUsers now? it will trigger rerender
+    // and we will end up in a vicious infinite loop.
+    // what is the solution? let's add dependency array as a second
+    // parameter of useEffect to have its callback function called
+    // only during the initial render.
+    setUsers(users);
     console.log(users);
   };
   useEffect(() => {
     getUsers();
-  });
+  }, []); // [] was added to be able to use setUsers in getUsers
   return (
     <>
       <h3>github users</h3>
+      <ul className='users'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
