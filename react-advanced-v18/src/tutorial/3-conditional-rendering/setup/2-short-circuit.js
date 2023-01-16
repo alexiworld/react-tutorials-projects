8c52a81de7daf2ac5afbd0dd1e511f8e1e6ff6a9
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-16 07:16:24 -0800

    91. Ternary Operator

--- react-advanced-v18/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-v18/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,14 +4,29 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
   const [text, setText] = useState('peter');
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+  const [isError, setIsError] = useState(false);
 
+  // Note: we cannot use 'if' to conditionally render because
+  // it does not return a value. We need to use either short-
+  // circuit operator or ternary operator as shown below. The
+  // difference between the two is that short-circuit operator
+  // display only if the condition is true while ternary always
+  // displays some but probably different output depending on
+  // the condition being true or false.
   return (
     <>
       <h1>{text || 'john doe'}</h1>
-      {!text && <h1>hello world</h1>}
-      {text && <h1>hello world, {text}!</h1>}
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
     </>
   );
 };
