a7ca5d92ffb5b07ac72bdeb5fa6f2f4d04a72f96
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-15 20:27:59 -0800

    86. useEffect - Cleanup Function. Explain how to use cleanup function to address problems with disappearing and reappering of components, as well multiple times registered same event listeners or other mess.

--- react-advanced-v18/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ react-advanced-v18/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -11,18 +11,27 @@ const UseEffectCleanup = () => {
     setSize(window.innerWidth);
   };
 
-  // [] is required to prevent adding a new event listener every
-  // time.
-  // to see the problem, resize the window a few times, seeing the
-  // new widths in the console log being printed out on every
-  // change, and then go to the Elements tab of Dev Tools, and Event
-  // Listeners subtab. There you will under 'resize' node see
-  // multiple Window event listeners with the line 10 (of the
-  // checkSize) being listed.
+  // Solving the problem with multiple registered event listeners
+  // by providing a cleanup function. The course instructor agrees
+  // that my own approach is more suitable for solving this problem
+  // but wants to show what the cleanup function is. The cleanup
+  // function as we gonna see when we start dealing with disappearing
+  // and reappearing of the components, will be very important.
+  // There will be components that will be gonna displayed and later
+  // removed. The problem is with that event listeners might be
+  // added. It is a good practice each time you set up useEffect to
+  // set up a cleanup function so you don't make a big mess by adding
+  // tons of subscriptions, tons of listeners.
   useEffect(() => {
+    console.log('useEffect');
     window.addEventListener('resize', checkSize);
-  }, []);
+    return () => {
+      console.log('cleanup');
+      window.removeEventListener('resize', checkSize);
+    };
+  });
 
+  console.log('render');
   return (
     <>
       <h1>window</h1>
