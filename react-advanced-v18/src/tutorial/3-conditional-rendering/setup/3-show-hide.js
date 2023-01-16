5d8c9374a6fa26045610f241259bdea64690262b
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-16 07:34:24 -0800

    92. Show/Hide Component

--- react-advanced-v18/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ react-advanced-v18/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,42 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button className='btn' onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {show && <Item />}
+    </>
+  );
+};
+
+const Item = () => {
+  console.log('create item'); // to show that during the
+  // conditional rendering with short-circuit operator
+  // each time we show item, a new item element is created.
+  // and hence [] as useEffect's 2nd param will not work.
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  // You will notice that useEffect callback is called
+  // every time we 'Show' the component (click the button)
+  // even though we have specified [] in useEffect. For
+  // this case we need to use the cleanup function.
+  useEffect(() => {
+    window.addEventListener('resize', checkSize);
+    return () => {
+      window.removeEventListener('resize', checkSize);
+    };
+  }, []);
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <h1>window</h1>
+      <h2>size: {size} px</h2>
+    </div>
+  );
 };
 
 export default ShowHide;
