8c3bb46f023e73ae320c58c3dcd866a815dca80f
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-15 16:37:46 -0800

    83. useEffect - Basics

--- react-advanced-v18/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-v18/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
-// by default runs after every re-render
+//useEffect is about the work done outside the component, from the video.
+
+// by default useEffect runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
+  useEffect(() => {
+    console.log('call useEffect');
+  });
+  console.log('render component');
   return <h2>useEffect Basics</h2>;
 };
 
