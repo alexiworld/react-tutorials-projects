c01a2445094f751aeedd1a3bbc2038953bf12407
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-15 21:02:25 -0800

    88. Multiple Returns - Basics

--- react-advanced-v18/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-v18/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
+  // in JS at encountering the first return
+  // the code will return.
+  return 'hello world';
   return <h2>multiple returns</h2>;
 };
 
