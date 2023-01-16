ca084b651511bd8a0238507cf990aad95e8ce47d
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-15 22:06:52 -0800

    89. Multiple Returns - Fetching Example.

--- react-advanced-v18/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-v18/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,15 +1,32 @@
 import React, { useState, useEffect } from 'react';
+
+// append X to test the error scenario 404 Not Found
 const url = 'https://api.github.com/users/QuincyLarson';
+
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState('default user');
 
   useEffect(() => {
+    setLoading(true);
     fetch(url)
-      .then((resp) => resp.json())
-      .then((user) => console.log(user))
-      .catch((error) => console.log(error));
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        } else {
+          setLoading(false);
+          setIsError(true);
+          throw new Error(resp.statusText);
+        }
+      })
+      .then((user) => {
+        console.log(user);
+        const { login } = user;
+        setUser(login);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error)); // this is for network error
   }, []);
 
   if (loading) {
