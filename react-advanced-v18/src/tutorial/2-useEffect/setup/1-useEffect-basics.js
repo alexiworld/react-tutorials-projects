a0cb3f2446e59ac724f04a09ba0bead92e9fda1e
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-15 16:59:08 -0800

    83. useEffect - Basics. More. Clicking the button will increment the value, causing a rerender and consequently updating the page title to represent the numner of new messages.

--- react-advanced-v18/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-v18/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,15 +1,36 @@
 import React, { useState, useEffect } from 'react';
-//useEffect is about the work done outside the component, from the video.
+// useEffect is about the work done outside the component, from the video.
+// useEffect is used when we want to setup side effect, and that is some
+// work outside of the component, e.g. data fetching, listening for events,
+// signing up for subscriptions, stuff along those lines. By default it will
+// run after every render and the way it works we pass a callback function
+// and whatever functionality we setup within that callback will run after every render.
 
 // by default useEffect runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
+  const [value, setValue] = useState(0);
   useEffect(() => {
     console.log('call useEffect');
+    document.title = `New Messages(${value})`;
   });
   console.log('render component');
-  return <h2>useEffect Basics</h2>;
+  return (
+    <>
+      <h1>{value}</h1>
+      <button
+        className='btn'
+        onClick={() =>
+          setValue((prevValue) => {
+            return prevValue + 1;
+          })
+        }
+      >
+        click me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
