9b16864599676d337ab22d16f50f2429eed1f727
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-16 19:58:57 -0800

    129. Reviews - Info and React-Icons

--- react-projects/03-reviews/setup/src/App.js
+++ react-projects/03-reviews/setup/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Review from './Review';
+// Importing icons:
+// https://react-icons.github.io/react-icons/
+import { FaGithubSquare } from 'react-icons/fa';
 function App() {
-  return <h2>reviews project setup</h2>;
+  return <h2>reviews project setup
+    <FaGithubSquare className='icon'/>
+  </h2>;
 }
 
 export default App;
