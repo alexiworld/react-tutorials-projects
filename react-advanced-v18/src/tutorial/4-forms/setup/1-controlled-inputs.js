1b6af41843449235930189aaddba0d01247738b9
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-21 21:27:08 -0800

    95. Controlled Inputs. Encountered react_devtools_backend.js:4012 Warning: You provided a alue prop to a form field without an onChange handler. This will render a read-only field. If the field should be mutable use defaultValue. Otherwise, set either onChange or eadOnly.

--- react-advanced-v18/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ react-advanced-v18/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,6 +6,9 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+
   const handleSubmit = (e) => {
     console.log('hello world');
     e.preventDefault();
@@ -16,11 +19,11 @@ const ControlledInputs = () => {
         <form className='form' onSubmit={handleSubmit}>
           <div className='form-control'>
             <label htmlFor='firstName'>Name: </label>
-            <input type='text' id='firstName' />
+            <input type='text' id='firstName' value={firstName} />
           </div>
           <div className='form-control'>
             <label htmlFor='email'>Email: </label>
-            <input type='text' id='email' name='email' />
+            <input type='text' id='email' name='email' value={email} />
           </div>
           {/* or define onClick attribute passing the handleSubmit fn */}
           <button type='submit'>add person</button>
