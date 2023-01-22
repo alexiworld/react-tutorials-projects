d30f6dcd8c494c2c7d9264d7453c4609e64846f9
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-21 22:31:53 -0800

    97. Multiple Inputs. The problem of adding a new field every time.

--- react-advanced-v18/src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ react-advanced-v18/src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -9,12 +9,18 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
+  const [age, setAge] = useState(0);
   const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (firstName && email) {
-      const person = { id: new Date().getTime().toString(), firstName, email };
+    if (firstName && email && age) {
+      const person = {
+        id: new Date().getTime().toString(),
+        firstName,
+        email,
+        age,
+      };
       console.log(person);
       setPeople((people) => {
         return [...people, person];
@@ -49,13 +55,24 @@ const ControlledInputs = () => {
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
+          <div className='form-control'>
+            <label htmlFor='age'>Age : </label>
+            <input
+              type='text'
+              id='age'
+              name='age'
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
           <button type='submit'>add person</button>
         </form>
         {people.map((person, index) => {
-          const { id, firstName, email } = person;
+          const { id, firstName, email, age } = person;
           return (
             <div className='item' key={id}>
               <h4>{firstName}</h4>
+              <h4>{age} years</h4>
               <p>{email}</p>
             </div>
           );
