a2d4f2fa7072b05b52f308e13f4e652661f09c53
Alexi Jordanov <alexiworld@yahoo.com>
2023-01-21 21:22:05 -0800

    94. Form Basics

--- react-advanced-v18/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ react-advanced-v18/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,28 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const handleSubmit = (e) => {
+    console.log('hello world');
+    e.preventDefault();
+  };
+  return (
+    <>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='firstName'>Name: </label>
+            <input type='text' id='firstName' />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Email: </label>
+            <input type='text' id='email' name='email' />
+          </div>
+          {/* or define onClick attribute passing the handleSubmit fn */}
+          <button type='submit'>add person</button>
+        </form>
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
