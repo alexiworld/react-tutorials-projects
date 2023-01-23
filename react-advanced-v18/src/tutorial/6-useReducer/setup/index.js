ee5a51874b7394caf8232a84174891e2db5bdfe6
alexiworld <alexiworld@yahoo.com>
2023-01-23 14:07:00 -0800

    104. useReducer - Remove Item. step#2 - component and reducer split in separate files.

--- react-advanced-v18/src/tutorial/6-useReducer/setup/index.js
+++ react-advanced-v18/src/tutorial/6-useReducer/setup/index.js
@@ -1,41 +1,7 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
-// reducer function
-const reducer = (state, action) => {
-  console.log(state, action);
-  // YOU ALWAYS WANT TO RETURN SOME STATE IF YOU WANT
-  // YOUR FUNCTIONALITY TO WORK!!! 
-  if (action.type === 'ADD_ITEM') {
-    const newPeople = [...state.people, action.payload]
-    return {...state, 
-      people: newPeople, 
-      isModalOpen: true, 
-      modalContent: 'item added'
-    };
-  };
-  if (action.type === 'NO_VALUE') {
-    return {...state, 
-      isModalOpen: true, 
-      modalContent: 'please enter value'
-    };
-  }
-  if (action.type === 'REMOVE_ITEM') {
-    const newPeople = state.people 
-      .filter((person) => person.id !== action.payload);
-
-    return {...state, 
-      people: newPeople, 
-      isModalOpen: true, 
-      modalContent: 'item removed'
-    };
-  }
-  if (action.type === 'CLOSE_MODAL') {
-    return {...state, isModalOpen: false};
-  }
-  //return state;
-  throw new Error('no matching action type');
-}
+import { reducer } from './reducer';
 const defaultState = {
   people: [],
   isModalOpen: false, 
