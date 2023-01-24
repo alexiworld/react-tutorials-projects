058c69a4154c1df00360dec025ebe1766703acc8
alexiworld <alexiworld@yahoo.com>
2023-01-23 21:13:32 -0800

    108. Custom Hooks - useFetch. This is a way to create reusable common functions.

--- react-advanced-v18/src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ react-advanced-v18/src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from 'react'
+ import React, { useState, useEffect } from 'react'
 import { useFetch } from './2-useFetch'
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
 
+// Create custom hook is like creating a reusable code. For
+// example, you can call useFetch in many places with different
+// URLs and get different loading state indicator and products.
+
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
-
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    getProducts()
-  }, [url])
+  const {loading, products} = useFetch(url);
   console.log(products)
   return (
     <div>
-      <h2>{loading ? 'loading...' : 'data'}</h2>
+      <h2>{loading ? 'loading...' : 
+          products.map((product) => {
+            return (<>
+            <div key={product.id}>
+              <h4>{product.fields.name}</h4>
+            </div>
+            </>);
+          })
+        }</h2>
     </div>
   )
 }
