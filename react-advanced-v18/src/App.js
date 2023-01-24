bee0b60241eea1dcd4986c22e822f37ad5c955ad
alexiworld <alexiworld@yahoo.com>
2023-01-23 19:10:41 -0800

    105. Prop Drilling

--- react-advanced-v18/src/App.js
+++ react-advanced-v18/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 // If a directory is specified, Node.js will look
 // for index.js to load the default object.
-import Setup from './tutorial/6-useReducer/setup';
-
+import Setup from './tutorial/7-prop-drilling/setup/1-prop-drilling';
 function App() {
   return (
     <div className='container'>
