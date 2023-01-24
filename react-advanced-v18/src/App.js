97c68600da00b064e14257da7b1be05bea26df63
alexiworld <alexiworld@yahoo.com>
2023-01-23 21:38:22 -0800

    109. PropTypes - Setup.

--- react-advanced-v18/src/App.js
+++ react-advanced-v18/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // If a directory is specified, Node.js will look
 // for index.js to load the default object.
-import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example';
+import Setup from './tutorial/10-prop-types/setup';
 function App() {
   return (
     <div className='container'>
--- react-advanced-v18/src/tutorial/10-prop-types/setup/Product.js
+++ react-advanced-v18/src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({image, name, price}) => {
+  // Cannot read properties of undefined because one of
+  // the images is undefined. This will not be the case
+  // when the price is undefined, where an empty string
+  // will be rendered.
+  return 
+  (<article className='product'>
+    <img src={image.url} alt={name}></img>
+    <h4>{name}</h4>
+    <p>{price}</p>
+    </article>);
 };
 
 export default Product;
+ 
