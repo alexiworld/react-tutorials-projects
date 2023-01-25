941c6e469ea42b4fd0d5b39f0ab226aa53dc24bb
alexiworld <alexiworld@yahoo.com>
2023-01-24 19:34:56 -0800

    117. useCallback - Fetch Example. Why adding getProducts to the 2nd param of useEffect? Because otherwise there is supposed to see some error reported in the shell. Added a comment with the expected error text.

--- react-advanced-v18/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ react-advanced-v18/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -10,11 +10,16 @@ export const useFetch = (url) => {
       const products = await response.json()
       setProducts(products)
       setLoading(false)
-    }
+    };
 
+    // React Hook useEffect has a missing dependency: 'getProducts'.
+    // Either include it or remove the dependency array react-hook/
+    // exhaustive-deps.
+    //
     // Adding getProducts to the array with variables, which
     // value change will trigger re-run of the useEffect fn
-    // WILL CREATE an INFINITE LOOP. 
+    // WILL CREATE an INFINITE LOOP. To work-around this new
+    // issue we could wrap getProducts in a useCallback.
     useEffect(() => {
       getProducts()
     }, [url, getProducts])
